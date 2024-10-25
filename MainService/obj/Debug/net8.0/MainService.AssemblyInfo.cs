92da6b0213bd43f574d7b558c1b2681867b4b580
jaouad mhamdi <98467197+jm-hamdi@users.noreply.github.com>
2024-10-25 15:59:22 +0100

    MainService

--- MainService/obj/Debug/net8.0/MainService.AssemblyInfo.cs
+++ MainService/obj/Debug/net8.0/MainService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99d31ea2599617fca9b5eac1033cab3d0d7b10f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
