c99d31ea2599617fca9b5eac1033cab3d0d7b10f
jaouad mhamdi <98467197+jm-hamdi@users.noreply.github.com>
2024-10-25 15:58:17 +0100

    ProductService updated

--- ProductService/obj/Debug/net8.0/ProductService.AssemblyInfo.cs
+++ ProductService/obj/Debug/net8.0/ProductService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55982b4fc789ff6399f44cbfbcf11b5d22d3dc16")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
